d13325fdbf6f0f7d25237cc8d6181010de8e0649
Johan Segerfeldt <johan.segerfeldt@me.com>
2021-06-28 15:40:22 +0200

    Adjust the image height to maintain proportions

--- screens/ShowScreen.tsx
+++ screens/ShowScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Image, ScrollView, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Dimensions, Image, ScrollView, Text, View } from 'react-native';
 import HTMLView from 'react-native-htmlview';
 
 export interface Show {
@@ -15,15 +16,25 @@ export interface Show {
 const ShowScreen = (props: any) => {
   const show = props.route.params as Show
 
+  const [imageHeight, setImageHeight] = useState(200)
+  const width = Dimensions.get('window').width
+  updateImageHeight()
+
   props.navigation.setOptions({title: `${show.name} (${show.rating ?? 'not rated'})`});
 
   return (
     <ScrollView>
-      <Image style={{flex: 1}} source={{uri: show.image ?? '', height: 200}} />
+      {show.image && <Image style={{flex: 1}} source={{uri: show.image ?? '', height: imageHeight, width: width}} />}
       <Text>{show.channel} {show.year} {show.country && `(${show.country})`}</Text>
       <HTMLView value={show.summary ?? '<p><em>No summary was returned</em></p>'} stylesheet={{p: {margin: 15}}} />
     </ScrollView>
   )
+
+  function updateImageHeight() {
+    if (!show.image) return
+
+    Image.getSize(show.image, (_width, _height) => setImageHeight(width / _width * _height));
+  }
 }
 
 export default ShowScreen
